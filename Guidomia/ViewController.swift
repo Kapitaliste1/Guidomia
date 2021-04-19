e14bb09d158446820a4334a760c983505f5dce7f
Jonathan Ngabo <jonathan@sidekickinteractive.com>
2021-04-19 01:33:27 -0400

    added initial project files

---
+++ Guidomia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia
+//
+//  Created by Jonathan Ngabo on 2021-04-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
